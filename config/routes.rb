656b1317f49b6c32189b28ab3b0c22bdb3b85623
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-12-13 10:05:45 +0000

    [Update]コントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'post_comments/create'
+  get 'post_comments/destroy'
+  get 'posts/show'
+  get 'posts/index'
+  get 'posts/create'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
+  get 'users/show'
+  get 'users/index'
+  get 'users/edit'
+  get 'users/update'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
