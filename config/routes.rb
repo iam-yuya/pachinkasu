385d18386959d4efafe468f319ec9da178ed0f3f
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-12-30 05:11:20 +0000

    [Add]いいね一覧表示用のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,21 +6,24 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  # deviseに新しくルーティングを追加
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-  end
-
-  resources :posts, only: [:index,:create,:show, :edit, :destroy, :update] do
-    resources :post_comments, only: [:create, :destroy]
-    resource :favorites, only: [:create, :destroy]
   end
 
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
+    member do
+      get :favorites
+    end
   end
 
+  resources :posts, only: [:index,:create,:show, :edit, :destroy, :update] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
